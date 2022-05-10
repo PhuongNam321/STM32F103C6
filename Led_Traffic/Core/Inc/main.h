4ba9526d7a3d039a93ee36fdb03410d2fd02e8a2
PhuongNam321 <nam.nguyenvoicon2001@hcmut.edu.vn>
2022-05-10 11:13:23 +0700

    frist commit

--- Led_Traffic/Core/Inc/main.h
+++ Led_Traffic/Core/Inc/main.h
@@ -57,6 +57,46 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED_1_Pin GPIO_PIN_2
+#define LED_RED_1_GPIO_Port GPIOA
+#define LED_YELLOW_1_Pin GPIO_PIN_3
+#define LED_YELLOW_1_GPIO_Port GPIOA
+#define LED_GREEN_1_Pin GPIO_PIN_4
+#define LED_GREEN_1_GPIO_Port GPIOA
+#define LED_RED_2_Pin GPIO_PIN_5
+#define LED_RED_2_GPIO_Port GPIOA
+#define LED_YELLOW_2_Pin GPIO_PIN_6
+#define LED_YELLOW_2_GPIO_Port GPIOA
+#define LED_GREEN_2_Pin GPIO_PIN_7
+#define LED_GREEN_2_GPIO_Port GPIOA
+#define SEG_0_Pin GPIO_PIN_0
+#define SEG_0_GPIO_Port GPIOB
+#define SEG_1_Pin GPIO_PIN_1
+#define SEG_1_GPIO_Port GPIOB
+#define SEG_2_Pin GPIO_PIN_2
+#define SEG_2_GPIO_Port GPIOB
+#define SEG_10_Pin GPIO_PIN_10
+#define SEG_10_GPIO_Port GPIOB
+#define SEG_11_Pin GPIO_PIN_11
+#define SEG_11_GPIO_Port GPIOB
+#define SEG_12_Pin GPIO_PIN_12
+#define SEG_12_GPIO_Port GPIOB
+#define SEG_13_Pin GPIO_PIN_13
+#define SEG_13_GPIO_Port GPIOB
+#define SEG_3_Pin GPIO_PIN_3
+#define SEG_3_GPIO_Port GPIOB
+#define SEG_4_Pin GPIO_PIN_4
+#define SEG_4_GPIO_Port GPIOB
+#define SEG_5_Pin GPIO_PIN_5
+#define SEG_5_GPIO_Port GPIOB
+#define SEG_6_Pin GPIO_PIN_6
+#define SEG_6_GPIO_Port GPIOB
+#define SEG_7_Pin GPIO_PIN_7
+#define SEG_7_GPIO_Port GPIOB
+#define SEG_8_Pin GPIO_PIN_8
+#define SEG_8_GPIO_Port GPIOB
+#define SEG_9_Pin GPIO_PIN_9
+#define SEG_9_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
